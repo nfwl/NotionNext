d563b1f1dca48e8842c0f851a2da937f4c3983d2
nfwl <96956500+nfwl@users.noreply.github.com>
2024-06-08 14:21:23 +0800

    Update Footer.js

--- themes/movie/components/Footer.js
+++ themes/movie/components/Footer.js
@@ -11,14 +11,14 @@ export const Footer = (props) => {
         <DarkModeButton className='text-center pt-4'/>
 
         <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
-            <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.</div>
+            <div className='text-center'> 本站含有成人内容，未满18岁者请勿进入，否则后果自负！ </div>
             <div className="md:p-0 text-center md:text-right text-xs">
                 {/* 右侧链接 */}
                 {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
                 {siteConfig('BEI_AN') && (<a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">{siteConfig('BEI_AN')} </a>)}
                 <span className='dark:text-gray-200 no-underline ml-4'>
-                    Powered by
-                    <a href="/" className=' hover:underline'> VCStatic {siteConfig('VERSION')}  </a>
+                    声明：
+                    <a href="/" className=' hover:underline'> 我们立足于美利坚合众国，对全球华人服务，受北美法律保护。 </a>
                 </span>
             </div>
         </div>
